0da059f4803b6a4df93a1f094e7c34a7d8f128bb
Daisuke Konishi <skydai1151@gmail.com>
2021-10-19 00:54:10 +0900

    環境変数で指定したポート番号をnumberとして扱えるように型定義ファイルの読み込みを追加

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,7 +3,7 @@ import { ConnectionOptions } from "typeorm-seeding";
 const DBConfig: ConnectionOptions = {
   type: "mysql",
   host: process.env.DB_HOST,
-  port: 3306,
+  port: process.env.DB_PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
